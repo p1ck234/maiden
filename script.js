16a6bbde3c9e0852d937056121d1248a51bc71f7
p1ck23 <danil-vetrov47@mail.ru>
2024-03-16 15:47:30 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const auth = () => {
+  const inputElement = document.querySelector("#authField");
+  const pass = "lovearina";
+  if (inputElement.value === pass) {
+    alert("А ты молодец!\nСама догадалась?");
+    renderQust();
+  } else {
+    alert("PIN неправильный.\nПопробуй еще раз");
+    inputElement.value = "";
+  }
+};
+
+const renderQust = () => {
+  const mainElement = document.querySelector(".main");
+  const random = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1);
+    return Math.floor(rand);
+  };
+  const authHtml = `<h1 class="main__title">Будешь моей <span style="color: transparent;">женой</span>?</h1>
+    <img src="./src/img/wait__answr.gif" alt="" />
+    <div class="main__box">
+      <button id="btnYes" class="main__button">Да</button>
+      <button id="btnNo" class="main__button">Нет</button>
+    </div>`;
+  mainElement.innerHTML = authHtml;
+
+  const buttonNoElement = document.querySelector("#btnNo");
+  const buttonYesElement = document.querySelector("#btnYes");
+  buttonNoElement.addEventListener("mouseenter", () => {
+    buttonNoElement.style.left = `${random(0, 90)}%`;
+    buttonNoElement.style.top = `${random(0, 90)}%`;
+  });
+
+  buttonNoElement.addEventListener("click", () => {
+    renderNo();
+  });
+
+  buttonYesElement.addEventListener("click", () => {
+    renderYes();
+  });
+};
+
+const renderYes = () => {
+  const authHtml = `<main class="main">
+  <h1 class="main__title">Ура!</h1>
+  <img src="./src/img/btn__yes.gif" alt="" /> 
+</main>`;
+  mainElement.innerHTML = authHtml;
+};
+
+const renderNo = () => {
+  const authHtml = ` <main class="main">
+    <h1 class="main__title">Окак</h1>
+    <img src="./src/img/btn__no.gif" alt="" /> 
+  </main>`;
+  mainElement.innerHTML = authHtml;
+};
+const mainElement = document.querySelector(".main");
+const authField = document.querySelector("#authField");
+const authButton = document.querySelector("#authButton");
+
+authField.addEventListener("keypress", function (e) {
+  var key = e.which || e.keyCode;
+  if (key === 13) {
+    authButton.click();
+  }
+});
